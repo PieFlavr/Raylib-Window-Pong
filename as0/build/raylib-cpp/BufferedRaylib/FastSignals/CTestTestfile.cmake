acbdffa2c0c1b29e8a53129c7cf521f235f9a01a
PieFlavr <lucasadampinto@gmail.com>
2025-01-23 13:12:56 -0800

    Removed build folder.

--- as0/build/raylib-cpp/BufferedRaylib/FastSignals/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/lucas/Desktop/Repo Clones/CS381-AS0/as0/raylib-cpp/BufferedRaylib/FastSignals
-# Build directory: /mnt/c/Users/lucas/Desktop/Repo Clones/CS381-AS0/as0/build/raylib-cpp/BufferedRaylib/FastSignals
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("libfastsignals")
