0e276e78bc0f8683a814ae327332e063c29fdadf
PieFlavr <lucasadampinto@gmail.com>
2025-02-06 21:07:30 -0800

    Set up audio basics and got all audio to properly load+play!

--- as1/src/as1.cpp
+++ as1/src/as1.cpp
@@ -1,55 +1,74 @@
-/**
- * @file as0.cpp
- * @author Lucas Pinto
- * @brief Code submission for Assignment 0 consisting of a resizable window of requested title, (initially) centered text, 
- *        and a toggleable "TV Logo Bounce" animation with rainbow colors.
- * @version 0.1
- * @date 2025-01-27
- * 
- * @copyright Copyright (c) 2025
- * 
- */
 #include "raylib-cpp.hpp"
-#include "VolumeControl.h"
+#include "VolumeControl.h" 
 
 #include <iostream>
 #include <string>
 #include <cmath>
 
-#define DEFAULT_FPS 60
+#define DRAW_FPS 60
 
-raylib::Music ping;
-
+raylib::Sound ping; //Using raylib::Sound since shorter sound bites (loaded into memory apparently)
+raylib::Music dialogue, music; //Using raylib::Music since plays for longer (disk streaming apparently)
+                                //Judgement says to put dialogue as music since it loops forever anyway.
 void PingButton(){
- // ijiji
+    ping.Play(); //Feature #4 -Sound Effect on Button Press (10 points)
 }
 
 int main(){
-    raylib::Window window(300,350,"Hello World");
+
+    double lastDrawTime = 0;
+    double drawDelta = 1.0 / DRAW_FPS; 
+
+    raylib::Window window(300,350,"CS381 - Assignment 1"); //Feature #1 - Window Title (1 pts)
     window.SetState(FLAG_WINDOW_RESIZABLE);
-    raylib::AudioDevice audio;
 
-    SetTargetFPS(DEFAULT_FPS); //For preventing too much resource usage, artifacting, and resizing issues.
+    raylib::AudioDevice defaultDevice;
 
-    raylib::Text text;
-    text.SetText("Hello World");
-    text.SetFontSize(30);
+    //Feature #3 - Load 3 Audio Files (30 pts)
+    music.Load("../../assets/audio/price-of-freedom.mp3");  //Sound Effect (10 pts)
+    dialogue.Load("../../assets/audio/crowd.wav"); //Dialogue (10 pts)
+    ping.Load("../../assets/audio/ping.wav");  //Music (10pts)
+    /*
+        AGHHHH THIS HURT MY BRAIN
+        Was looking at the raylib documentation, forgetting we're using the raylib-cpp wrapper.
+        Apparently raylib handles loading into Sound different than raylib-cpp.
+        
+        That, and the utter confusion between using raylib vs. raylib-cpp namespaces
+        hurt my brain for a good 30 minutes.
+        
+        I see how it works now, with OG raylib stuff accessible w/o name specifies
+        and raylib-cpp stuff accessible via raylib:: namespace.
+     */
+
+    dialogue.SetLooping(false);
+    music.SetLooping(false);
+
+    dialogue.SetVolume(0.5); //Set each to default volumes
+    music.SetVolume(0.5);
+    ping.SetVolume(0.5);
+
+    dialogue.Play();
+    music.Play();
 
     auto guiState = InitGuiVolumeControl();
-    
-    ping.Load("../../assets/audio/ping.wav");
-    ping.SetLooping(false);
-    ping.Play();
 
     while(!window.ShouldClose()){
 
-        window.BeginDrawing();
-
-        ClearBackground(BLACK);
-        GuiVolumeControl(&guiState);
-        ping.Update();
+        double currentTime = GetTime();
         
-        window.EndDrawing(); 
+        dialogue.Update();
+        music.Update();
+
+        // Rendering Block
+        if(currentTime - lastDrawTime >= drawDelta){
+            window.BeginDrawing();
+
+            ClearBackground(BLACK);
+            GuiVolumeControl(&guiState); //Feature #2 - Load the Volume Control GUI
+            
+            window.EndDrawing(); 
+        }
+        
 
     }
 }
