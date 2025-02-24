b77701fe7dd3f49ce14cb1271055199cff9ae5f2
PieFlavr <lucasadampinto@gmail.com>
2025-02-23 23:27:19 -0800

    Improved freebird.

--- as3/src/as3.cpp
+++ as3/src/as3.cpp
@@ -28,7 +28,8 @@
  #define INITIAL_FONT_SPACING 2
 
  #define MAX_SPEED 300
- #define CAMERA_LERP (0.02)
+ #define CAMERA_LERP (0.03)
+ #define CAR_LERP (0.05)
 
  #define MAX_SPEED 300
  #define Y_HEADING_LERP (0.1)
@@ -41,6 +42,9 @@
  #define X_HEADING_LERP (0.02)
  #define X_HEAD_LIMIT (45.0*DEG2RAD)
  #define DELTA_COMPENSATOR 100.0
+
+ #define PLANE_WIDTH 10000
+ #define PLANE_LENGTH 10000
  
  
  template<typename T> //Cool type validation!
@@ -181,7 +185,7 @@ int main()
         0
     };
 
-    raylib::Model grass = raylib::Mesh::Plane(1000, 1000, 10, 10).LoadModelFrom();
+    raylib::Model grass = raylib::Mesh::Plane(PLANE_WIDTH, PLANE_LENGTH, 100, 100).LoadModelFrom();
     raylib::Texture grassTexture = raylib::Texture("../../assets/textures/grass.png");
     grass.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = grassTexture;
 
@@ -190,6 +194,12 @@ int main()
 
     raylib::Model wheel = raylib::Model("../../assets/Kenny Car Kit/wheel-default.glb");
     wheel.transform = raylib::Matrix::Identity().Scale(DEFAULT_SCALE);
+
+    raylib::Model cow = raylib::Model("../../custom_assets/as2/Cow.glb");   
+    cow.transform = raylib::Matrix::Identity().Scale(DEFAULT_SCALE);    // From... https://www.fab.com/listings/fbd8c8b0-fc59-460e-adb8-c7ab8f19a6f9
+
+    raylib::Model rocket = raylib::Model("../../assets/Kenny Space Kit/rocketA.glb"); 
+    rocket.transform = raylib::Matrix::Identity().Scale(DEFAULT_SCALE);
 
     raylib::AudioDevice defaultDevice;
     raylib::Music freebird;
@@ -206,6 +216,7 @@ int main()
         // ===========================================================
 
             double logicDelta = GetFrameTime();
+            double freebird_factor = freebird_volume/DEFAULT_VOLUME;
 
             // I think this counts as unbuffered input??? 
             // Feature #5 - Use Unbuffered Input (10 points total)
@@ -220,28 +231,29 @@ int main()
             keyDPressed = keyDPressed ? keyDPressed : raylib::Keyboard::IsKeyDown(KEY_D);
             keySpacePressed = keySpacePressed ? keySpacePressed : raylib::Keyboard::IsKeyDown(KEY_SPACE);
 
+
             if(timer <= 0){ //Feature #5A - Incremenenting, not Continuous Values (5pts)
-                double camera_lerp_ratio = std::min( CAMERA_FOV_LERP * DELTA_COMPENSATOR * logicDelta, 1.0);
-                
-                if(keyDPressed){ //Feature 5F - Pressing D Increases Car's Heading (2 points)
+                double camera_lerp_ratio = std::min( pow(freebird_factor,2) * CAMERA_FOV_LERP * DELTA_COMPENSATOR * logicDelta, 1.0);
+                double car_vel_lerp_ratio = std::min(CAR_LERP*DELTA_COMPENSATOR * logicDelta, 1.0);
+                if(keyDPressed){ //Feature #5F - Pressing D Increases Car's Heading (2 points)
                     car_kinematics.rot.y -= Y_HEADING_ANGLE_INCREMENT*DEG2RAD;
                     car_kinematics.rot.y = fmod(car_kinematics.rot.y, 360*DEG2RAD);
                     keyDPressed = false;
-                } else if(keyAPressed){ //Feature 5E - Pressing A Increases Car's Heading (2 points)
+                } else if(keyAPressed){ //Feature #5E - Pressing A Increases Car's Heading (2 points)
                     car_kinematics.rot.y += Y_HEADING_ANGLE_INCREMENT*DEG2RAD;
                     car_kinematics.rot.y = fmod(car_kinematics.rot.y, 360*DEG2RAD);
                     keyAPressed = false;
                 } 
     
-                if(keyWPressed){ //Feature 5C - Pressin W Increases Car Velcoity (1 point)
-                    car_kinematics.vel = lerp(car_kinematics.vel, MAX_SPEED, 0.1);
+                if(keyWPressed){ //Feature #5C - Pressin W Increases Car Velcoity (1 point)
+                    car_kinematics.vel = lerp(car_kinematics.vel, MAX_SPEED+(std::pow(3,10*freebird_factor)-1), car_vel_lerp_ratio);
                     camera.SetFovy(std::max(lerp(camera.GetFovy(), CAMERA_MAXIMUM_FOV, camera_lerp_ratio),CAMERA_MINIMUM_FOV));
                     keyWPressed = false;
-                } else if(keySPressed){ //Feature 5D - Pressing S Decreases Car Velcoity (1 point)
-                    car_kinematics.vel = lerp(car_kinematics.vel, -MAX_SPEED, 0.1);
-                    keySPressed= false;
+                } else if(keySPressed){ //Feature #5D - Pressing S Decreases Car Velcoity (1 point)
+                    car_kinematics.vel = lerp(car_kinematics.vel, -MAX_SPEED, car_vel_lerp_ratio);
+                    keySPressed = false;
                 } else {
-                    car_kinematics.vel = lerp(car_kinematics.vel, 0, 0.1);
+                    car_kinematics.vel = lerp(car_kinematics.vel, 0, 0.25*car_vel_lerp_ratio);
                     camera.SetFovy(lerp(camera.GetFovy(), CAMERA_MINIMUM_FOV, camera_lerp_ratio));
                     //Feature #5B - Car Continues to Move even w/o active control (30 points)
                 }
@@ -260,7 +272,7 @@ int main()
                     car_kinematics.rot.x = lerp(car_kinematics.rot.x, 0, X_heading_lerp_ratio);
                 }
 
-                if(keySpacePressed){ //Feature 5G - Pressing Space Resets Car's Velocity (4 points)
+                if(keySpacePressed){ //Feature #5G - Pressing Space Resets Car's Velocity (4 points)
                     car_kinematics.vel = 0;
                     keySpacePressed = false;
                 }
@@ -272,7 +284,7 @@ int main()
             }
 
             
-            //Update Kinematics
+            //Feature #3 - Kinematics Physics (10 points), 3D too!
             //to be honest I'm not if I'm doing it right, but it seems to be working so :p
             //too lazy to work out the algebra so I just swapped sin/cos from standard spherical coordinates to whatever the heck raylib lieks
             car_kinematics.pos.x += car_kinematics.vel * sin(car_kinematics.rot.y) * cos(car_kinematics.rot.x) * logicDelta;
@@ -301,12 +313,15 @@ int main()
 
             timer -= window.GetFrameTime()*1000;
             
-            if(car_kinematics.vel > (MAX_SPEED/2) && (car_kinematics.rot.x < -10*DEG2RAD || car_kinematics.rot.x > 10*DEG2RAD)){
-                freebird_volume = lerp(freebird_volume, DEFAULT_VOLUME, 0.0001*DELTA_COMPENSATOR*logicDelta);
+            float neg_velocity_factor = MAX_SPEED/std::max(car_kinematics.vel,(double)(MAX_SPEED*0.90));
+            float pos_velocity_factor = std::max(car_kinematics.vel,(double)(MAX_SPEED*0.90))/MAX_SPEED;
+            float heading_factor = std::max(std::abs(car_kinematics.rot.x)/(X_HEAD_LIMIT),0.1);
+
+            if(car_kinematics.vel > (MAX_SPEED/2) && car_kinematics.pos.y != 0){
+                freebird_volume = lerp(freebird_volume, DEFAULT_VOLUME, 0.0003*heading_factor*pos_velocity_factor*DELTA_COMPENSATOR*logicDelta);
                 freebird.SetVolume(freebird_volume);
             } else {
-                float velocity_factor = MAX_SPEED/std::max(car_kinematics.vel,(double)(MAX_SPEED*0.90));
-                freebird_volume = lerp(freebird_volume, 0, velocity_factor*0.005*DELTA_COMPENSATOR*logicDelta);
+                freebird_volume = lerp(freebird_volume, 0, neg_velocity_factor*0.005*DELTA_COMPENSATOR*logicDelta);
                 freebird.SetVolume(freebird_volume);
             }
 
@@ -318,6 +333,19 @@ int main()
                 freebird.Play();
                 freebird_playing = true;
                 std::cout << "Freebird is playing" << std::endl;
+            }
+
+            if(freebird_factor >= 0.9999){
+                std::cout << "FREEBIRD IS AT MAX CAPACITY, CRASHING PROGRAM!!! (╯°□°）╯︵ ┻━┻" << std::endl;
+                camera.EndMode();
+                window.EndDrawing();
+                return 0;
+            } else if (freebird_factor >= 0.05){
+                //std::cout << "Position: " << car_kinematics.pos.x << " " << car_kinematics.pos.z << std::endl;
+                //std::cout << "Speed: " << car_kinematics.vel << std::endl
+                car_kinematics.pos.x = fmod(car_kinematics.pos.x, PLANE_WIDTH/2);
+                car_kinematics.pos.z = fmod(car_kinematics.pos.z, PLANE_LENGTH/2);
+                car_kinematics.pos.y = std::max(car_kinematics.pos.y, (float)0.0);
             }
 
             freebird.Update();
@@ -333,6 +361,13 @@ int main()
             sky.Draw();       //Feature #2 - Create Scene w/ a Car, Skybox, and Ground Plane (5 points)
             grass.Draw({});
             
+            if(freebird_playing){
+                DrawBoundedModel(cow, combine(rotate({0,1,0},180*DEG2RAD),translate({0,10,0}),car_transform));
+
+                if(freebird_factor > 0.5){
+                    DrawBoundedModel(rocket, combine(rotate({1,0,0},90*DEG2RAD),translate({0,10,-50}),car_transform));
+                }
+            }
             //EC Feature #2 - Add Four Wheels to the Car that FOllow (5 points)
             DrawBoundedModel(wheel, combine(translate({15, 0, 20}), car_transform));
             DrawBoundedModel(wheel, combine(rotate({0,1,0},180*DEG2RAD), translate({-15, 0, 20}), car_transform));
@@ -349,7 +384,7 @@ int main()
             Vector2(10,10), 
             INITIAL_FONT_SIZE/2, 
             INITIAL_FONT_SPACING, 
-            Color{0,0,0,(int)(255*std::max(1-(freebird_volume*10.0/DEFAULT_VOLUME),0.0))});
+            Color{0,0,0,(int)(255*std::max(1-(freebird_factor*50),0.0))});
 
             if(freebird_playing){
                 std::string freebird_text = TextFormat("freebird is playing... [%.2f%]", (freebird_volume/DEFAULT_VOLUME)*100.0);
@@ -360,6 +395,69 @@ int main()
                 INITIAL_FONT_SIZE/2, 
                 INITIAL_FONT_SPACING, 
                 Color{0,0,0,(int)(255*std::min((freebird_volume*10.0/DEFAULT_VOLUME),1.0))});
+
+                if(freebird_factor > 0.01){
+                    std::string freebird1_text = TextFormat("CONTAINING FREEBIRD TO BOUNDARY CONDITIONS... [%.2f%]", std::min((freebird_factor/0.05)*100.0,100.0));
+                    freebird1_text += "\n[FLYING UP/DOWN ACCELERATES FREEBIRD FACTOR]";
+                    const char* freebird1_text_cstr = freebird1_text.c_str();
+                    DrawTextEx(GetFontDefault(), 
+                    freebird1_text_cstr, 
+                    Vector2(10,0), 
+                    INITIAL_FONT_SIZE/2, 
+                    INITIAL_FONT_SPACING, 
+                    Color{0,0,0,(int)(255*std::min((freebird_volume*10.0/DEFAULT_VOLUME),1.0))});
+                } 
+
+                if(freebird_factor > 0.1){
+                    std::string freebird2_text;
+                    if(freebird_factor > 0.4){
+                        freebird2_text = "FREEBIRD IS ATTEMPTING TO ESCAPE";
+                    } else if (freebird_factor > 0.3){
+                        freebird2_text = "FREEBIRD IS CONTAINED..?";
+                    } else if (freebird_factor > 0.2){
+                        freebird2_text = "FREEBIRD IS CONTAINED...";
+                    } else {
+                        freebird2_text = "FREEBIRD IS CONTAINED";
+                    }
+                    const char* freebird2_text_cstr = freebird2_text.c_str();
+                    DrawTextEx(GetFontDefault(), 
+                    freebird2_text_cstr, 
+                    Vector2(10,INITIAL_FONT_SIZE), 
+                    INITIAL_FONT_SIZE/2, 
+                    INITIAL_FONT_SPACING, 
+                    Color{0,0,0,(int)(255*std::min((freebird_volume*10.0/DEFAULT_VOLUME),1.0))});
+                } 
+
+                if(freebird_factor > 0.5){
+                    std::string freebird3_text = TextFormat("FREEBIRD TO CRITICAL HORIZON BOUNDARY... [%.2f%]", ((freebird_factor/0.9)*100.0));
+                    if (freebird_factor > 0.6){
+                        freebird3_text += "\nFREEBIRD IS APPROACHING CRITICAL HORIZON BOUNDARY";
+                        if(freebird_factor > 0.8){
+                            freebird3_text += "\nFREEBIRD IS TOO FAST!!!";
+                        } else {
+                            freebird3_text += TextFormat("\nFREEBIRD IS AT VELOCITY: %.2f", car_kinematics.vel);
+                        }
+                        if(freebird_factor > 0.85){
+                            freebird3_text += "\n!!!BOUNDARY COLLAPSE IMMINENT!!!";
+                        }
+                        if(freebird_factor > 0.9){
+                            freebird3_text += "\n!!FREEBIRD ABOUT TO BREACH CONTAINMENT!!!";
+                        }
+                        if(freebird_factor > 0.99){
+                            freebird3_text += "\n!!!BOUDARY COLLAPSING!!!";
+                            for(int i = (int)((1-freebird_factor)*1000); i < 10; i++){
+                                freebird3_text += "\n!!!FREEBIRD IS ESCAPING!!!";
+                            }
+                        }
+                    }
+                    const char* freebird3_text_cstr = freebird3_text.c_str();
+                    DrawTextEx(GetFontDefault(), 
+                    freebird3_text_cstr, 
+                    Vector2(10,INITIAL_FONT_SIZE*2), 
+                    INITIAL_FONT_SIZE/2, 
+                    INITIAL_FONT_SPACING, 
+                    Color{0,0,0,(255*std::min((freebird_volume*10.0/DEFAULT_VOLUME),1.0))});
+                } 
             } 
 
         window.EndDrawing(); 
